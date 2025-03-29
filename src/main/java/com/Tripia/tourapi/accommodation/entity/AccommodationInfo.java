fe63b83bf9901e3acaa50825e547da267e1e7f1d
noah-wilson0 <127368908+noah-wilson0@users.noreply.github.com>
2025-03-29 18:20:03 +0900

    MODIFY: AccommodationInfo 수정
    /searchStay1 응답에서 실제 사용하는 데이터만 추려 AccommodationInfo 객체로 정의함.
    기존의 미완성 상태였던 AccommodationInfo를 TourAPI 응답 형식에 맞게 객체화함.

--- src/main/java/com/Tripia/tourapi/accommodation/entity/AccommodationInfo.java
+++ src/main/java/com/Tripia/tourapi/accommodation/entity/AccommodationInfo.java
@@ -1,17 +1,51 @@
 package com.Tripia.tourapi.accommodation.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table(name = "accommodation_info")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class AccommodationInfo {
 
     @Id
-    private Long id;
+    @Column(name = "content_id")
+    private String contentId;
 
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String code;
+
+
+    @Column(nullable = false)
     private String addr1;
+
     private String addr2;
+
+    private String image;
+
+    @Column(nullable = false,unique = true)
+    private String longitude; //경도
+
+    @Column(nullable = false,unique = true)
+    private String latitude;  //위도
+
+    private String tel;
+
+    public void changeContentId(String contentId) {
+        this.contentId = contentId;
+    }
+
+    public void changeTitle(String title) {
+        this.title = title;
+    }
 
 }
