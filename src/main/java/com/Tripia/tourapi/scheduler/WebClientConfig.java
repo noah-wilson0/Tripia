eaeefca259b1445bd5f4d7874f7dad9fa548b977
noah-wilson0 <127368908+noah-wilson0@users.noreply.github.com>
2025-03-29 18:37:52 +0900

    MODIFY: DataBufferLimitException 오류 해결
    AccommodationSyncService에서 발생한
    DataBufferLimitException 문제를 해결함.
    Spring WebClient는 기본 메모리 버퍼 크기를
    256KB로 설정함. 응답 데이터가 해당 크기를
    초과해 예외가 발생했으며, 메모리 버퍼 크기를
    10MB로 늘려 문제를 해결함.

--- src/main/java/com/Tripia/tourapi/scheduler/WebClientConfig.java
+++ src/main/java/com/Tripia/tourapi/scheduler/WebClientConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
@@ -22,6 +23,12 @@ public class WebClientConfig {
         return WebClient.builder()
                 .uriBuilderFactory(factory)
                 .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .exchangeStrategies(ExchangeStrategies
+                        .builder()
+                        .codecs(clientCodecConfigurer -> clientCodecConfigurer
+                                .defaultCodecs()
+                                .maxInMemorySize(10*1024*1024))
+                        .build())
                 .build();
     }
 }
